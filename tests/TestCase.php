797eb2d7789dd4efee4a05a558820b45ec49bc11
Yasuto Tanaka <yasutonaka@gmail.com>
2021-10-20 17:27:03 -0300

    test: Add test helper to work with reflections

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected $reflectionClass;
+
     /**
      * Creates the application.
      *
@@ -12,5 +14,33 @@ abstract class TestCase extends BaseTestCase
     public function createApplication()
     {
         return require __DIR__.'/../bootstrap/app.php';
+    }
+
+    protected function getReflection($class)
+    {
+        if ($this->reflectionClass) {
+            return $this->reflectionClass;
+        }
+
+        $this->reflectionClass = new ReflectionClass($class);
+
+        return $this->reflectionClass;
+    }
+
+    protected function getProtectedMethod($reflection, string $methodName)
+    {
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    protected function setProtectedPropertyValue($reflection, $mockedObj, string $property, $value)
+    {
+        $reflectionValue = $reflection->getProperty($property);
+
+        $reflectionValue->setAccessible(true);
+
+        $reflectionValue->setValue($mockedObj, $value);
     }
 }
