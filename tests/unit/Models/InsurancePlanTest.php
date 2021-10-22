99689bea531ab47e5a1c38bd5f842f64aeea4772
Yasuto Tanaka <yasutonaka@gmail.com>
2021-10-21 21:39:51 -0300

    test: Fix test after refactoring of InsurancePlan model

--- tests/unit/Models/InsurancePlanTest.php
+++ tests/unit/Models/InsurancePlanTest.php
@@ -2,39 +2,33 @@
 
 namespace Tests\Unit\Models;
 
-use App\Enums\Operation;
-use App\Models\AutoInsurancePlan;
-use App\Models\DisabilityInsurancePlan;
-use App\Models\HomeInsurancePlan;
-use App\Models\LifeInsurancePlan;
 use App\Models\UserInformation;
+use App\Processors\Operations\Operation;
 use App\Processors\RiskRules\AbstractRiskRuleHandler;
 use TestCase;
 
 class InsurancePlanTest extends TestCase
 {
-    public function test_rules()
+    /**
+     * @testWith
+     *      ["App\\Models\\AutoInsurancePlan"]
+     *      ["App\\Models\\DisabilityInsurancePlan"]
+     *      ["App\\Models\\HomeInsurancePlan"]
+     *      ["App\\Models\\LifeInsurancePlan"]
+     */
+    public function testRules($insurancePlan)
     {
-        $insurancePlans = [
-            AutoInsurancePlan::class,
-            DisabilityInsurancePlan::class,
-            HomeInsurancePlan::class,
-            LifeInsurancePlan::class
-        ];
+        $ui = $this->createMock(UserInformation::class);
+        $insurancePlan = new $insurancePlan($ui);
+        $rules = $this->getProtectedPropertyValue($insurancePlan, 'rules');
 
-        foreach ($insurancePlans as $insurancePlan) {
-            $ui = $this->createMock(UserInformation::class);
-            $insurancePlan = new $insurancePlan($ui);
-            $rules = $this->getProtectedPropertyValue($insurancePlan, 'rules');
+        $this->assertNotEmpty($rules);
 
-            $this->assertNotEmpty($rules);
-
-            foreach ($rules as $rule) {
-                $this->assertCount(3, $rule);
-                $this->assertTrue(is_subclass_of($rule[0], AbstractRiskRuleHandler::class, true));
-                $this->assertTrue(Operation::isValid($rule[1]));
-                $this->assertIsInt($rule[2]);
-            }
+        foreach ($rules as $rule) {
+            $this->assertCount(3, $rule);
+            $this->assertTrue(is_subclass_of($rule['rule'], AbstractRiskRuleHandler::class, true));
+            $this->assertTrue(is_subclass_of($rule['operation'], Operation::class));
+            $this->assertIsInt($rule['score']);
         }
     }
 }
